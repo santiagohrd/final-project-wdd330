875a51ef8858cc5d41b42bfa638c13246928bd17
SantiagoHrd <santiagohrd1@gmail.com>
2025-08-14 15:08:30 -0500

    testing

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
